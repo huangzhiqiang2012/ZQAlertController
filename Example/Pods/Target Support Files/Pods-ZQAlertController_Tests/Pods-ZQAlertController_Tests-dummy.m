83bfff1b09b4d097b81a30dca9e866b0e4304920
huangzhiqiang <huang_zhiqiang2012@126.com>
2019-01-01 21:00:01 +0800

    项目结构重构

--- Example/Pods/Target Support Files/Pods-ZQAlertController_Tests/Pods-ZQAlertController_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZQAlertController_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ZQAlertController_Tests
-@end
---
+++ Source/ZQAlertController.h
@@ -0,0 +1,19 @@
+//
+//  ZQAlertController.h
+//  ZQAlertController
+//
+//  Created by Darren on 2019/1/1.
+//  Copyright © 2019 Darren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZQAlertController.
+FOUNDATION_EXPORT double ZQAlertControllerVersionNumber;
+
+//! Project version string for ZQAlertController.
+FOUNDATION_EXPORT const unsigned char ZQAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZQAlertController/PublicHeader.h>
+
+
